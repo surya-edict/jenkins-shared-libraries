522845a985b958e981878a55ced1439271c52fc2
Surya Repo <84668485+surya-edict@users.noreply.github.com>
2025-01-28 01:20:17 +0530

    Update docker-build.groovy

--- vars/docker-build.groovy
+++ vars/docker-build.groovy
@@ -1,3 +1,3 @@
-def calt(String ProtectName, String ImageTag, String DockerHubUser){
+def call(String ProjectName, String ImageTag, String DockerHubUser){
   sh "docker build —t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
