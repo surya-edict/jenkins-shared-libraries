d768672fd9928b2cd8deade8dae2670d15ebed82
Surya Repo <84668485+surya-edict@users.noreply.github.com>
2025-01-28 00:57:17 +0530

    Update docker-cred.groovy

--- vars/docker-cred.groovy
+++ vars/docker-cred.groovy
@@ -1,8 +1,7 @@
 def call(){
-  stage('Push to Docker Hub') {
-            steps {
-                echo 'Push to Docker Hub'
-                withCredentials([usernamePassword(credentialsId: 'dockerhub-cred', passwordVariable: 'dockerhubPass', usernameVariable: 'dockerhubUser')]) {
-                    sh 'docker login -u $dockerhubUser -p $dockerhubPass'
+  echo 'Push to Docker Hub'
+  withCredentials([usernamePassword(credentialsId: 'dockerhub-cred', passwordVariable: 'dockerhubPass', usernameVariable: 'dockerhubUser')]) {
+    sh 'docker login -u $dockerhubUser -p $dockerhubPass'
 }
+
   
